cbb375ae8a69035165abcc4b745bdaa433cefc30
Sushil Deshmukh <sushil.deshmukh@joshsoftware.com>
2022-03-01 13:43:48 +0530

    header completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ShippingHeaderComponent } from './header/shipping-header/shipping-header.component';
+import { MainHeaderComponent } from './header/main-header/main-header.component';
 import { AppComponent } from './app.component';
+import { HeaderModule } from './header/header.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
