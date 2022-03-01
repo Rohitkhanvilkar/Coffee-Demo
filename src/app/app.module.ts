732197a1f6465a4201d12b03f222ada2a6bc5eff
Rohit Khanvilkar <rohit.khanvilkar1995@gmail.com>
2022-03-01 13:40:33 +0530

    footer completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { NewsletterComponent } from './newsletter/newsletter.component';
+import { ContactInfoComponent } from './contact-info/contact-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NewsletterComponent,
+    ContactInfoComponent,
   ],
   imports: [
     BrowserModule
